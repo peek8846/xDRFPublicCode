d4e210a4eb95a80ffbb3d74cbd0af2bd228e3db4
zaik <jonatan.waern.5948@student.uu.se>
2016-04-20 19:11:29 +0200

    Added marking of entry/exit xDRF for entry threads, fixed silly error where regions where marked as the opposite of their actual enclaveness in code

--- MarkXDRFRegions/MarkXDRFRegions.cpp
+++ MarkXDRFRegions/MarkXDRFRegions.cpp
@@ -95,8 +95,22 @@ namespace {
         virtual bool runOnModule(Module &M) {
             XDRFExtension &xdrfextended  = getAnalysis<XDRFExtension>();
 
-	    if (xdrfextended.nDRFRegions.size() == 0)
-                return false;
+	    // if (xdrfextended.nDRFRegions.size() == 0)
+            //     return false;
+
+            SmallPtrSet<Function*,4> entrypoints;
+
+            //Find the "main" function
+            Function *main = M.getFunction("main");
+            if (!main) {
+                PRINT << "No 'main' function detected, considering manually inputing the entry function\n";
+                PRINT << "Exiting\n";
+                assert(!"Did not detect main function");  
+            }
+            entrypoints.insert(main);
+
+            //Find other functions to analyze
+            findEntryPoints(M,entrypoints);
 
 	    Function *beginNDRF = createDummyFunction("begin_NDRF",M);
 	    Function *endNDRF = createDummyFunction("end_NDRF",M);
@@ -105,20 +119,94 @@ namespace {
 
 	    for (nDRFRegion * region : xdrfextended.nDRFRegions) {
                 for (Instruction * inst : region->beginsAt) {
-                    if (region->enclave)
-                        createDummyCall(endXDRF,inst);
-                    createDummyCall(beginNDRF,inst);
+                    if (!(region->enclave))
+                        createDummyCall(endXDRF,inst,0);
+                    createDummyCall(beginNDRF,inst,0);
                 }
                 for (Instruction * inst : region->endsAt) {
-                    if (region->enclave)
-                        createDummyCall(beginXDRF,inst,false);
-                    createDummyCall(endNDRF,inst,false);
+                    if (!(region->enclave))
+                        createDummyCall(beginXDRF,inst,false,0);
+                    createDummyCall(endNDRF,inst,false,0);
                 }
 	    }
+
+            for (Function * fun : entrypoints) {
+                VERBOSE_PRINT("Marking entry/exit xDRF regions in " << fun->getName() << "\n");
+                createDummyCall(beginXDRF,&*inst_begin(fun),true,0);
+                for (auto bit = inst_begin(fun),
+                         bet = inst_end(fun);
+                     bit != bet; ++bit) {
+                    if (isa<ReturnInst>(&*bit))
+                        createDummyCall(endXDRF,&*bit,0);
+                }
+            }
 
             return true;
         }
     private:
+
+        //Utility: Checks wether a given callsite contains a call
+        bool isNotNull(CallSite call) {
+            return call.isCall() || call.isInvoke();
+        }
+        
+        //Utility: Checks whether an instruction could be a callsite
+        bool isCallSite(Instruction* inst) {
+            return isNotNull(CallSite(inst));
+        }
+
+        //Finds the functions that may be the entry points of threads
+        //INPUT: The module to analyze and the set into which to insert
+        //the results
+        void findEntryPoints(Module &M, SmallPtrSet<Function*,4> &targetFunctions) {
+            VERBOSE_PRINT("Determining entry points for threads...\n");
+            //For each function that can spawn new threads, find the calls to that function
+            for (StringRef funName : threadFunctions) {
+                VERBOSE_PRINT("Finding functions used by " << funName << "\n");
+                Function *fun = M.getFunction(funName);
+                if (!fun) {
+                    VERBOSE_PRINT("was not used by module\n");
+                } else {
+                    for (auto call = fun->users().begin(); call != fun->users().end(); ++call) {
+                        DEBUG_PRINT("Examining use: " << **call << "\n");
+                        if (dyn_cast<Instruction>(*call) && isCallSite(dyn_cast<Instruction>(*call))) {
+                            CallSite callsite(*call);
+                            for (int opnum = 0; opnum < callsite.getNumArgOperands(); ++opnum) {
+                                Value *funcOp = callsite.getArgOperand(opnum);
+                                DEBUG_PRINT("Examining argument: " << *funcOp << "\n");
+                                //Try to resolve the value into a proper function
+                                while (!isa<Function>(funcOp)) {
+                                    if (isa<ConstantExpr>(funcOp)) {
+                                        ConstantExpr *constOp = dyn_cast<ConstantExpr>(funcOp);
+                                        if (constOp->isCast()) {
+                                            funcOp = constOp->getAsInstruction();
+                                            CastInst *castOp = dyn_cast<CastInst>(funcOp);
+                                            Value* tempOp = funcOp;
+                                            funcOp = castOp->getOperand(0);
+                                            delete(tempOp);
+                                        }
+                                    } else if (isa<CastInst>(funcOp)) {
+                                        CastInst *castOp = dyn_cast<CastInst>(funcOp);
+                                        funcOp = castOp->getOperand(0);
+                                    }
+                                    else { //Unable To Resolve
+                                        DEBUG_PRINT("Failed to resolve argument " << *funcOp
+                                                    << " to a function, remaining type is:" << typeid(*funcOp).name() << "\n");
+                                        break;
+                                    }
+                                }
+                                if (auto spawnFunc = dyn_cast<Function>(funcOp)) {
+                                    VERBOSE_PRINT("Targeting function: " << spawnFunc->getName() << "\n");
+                                    targetFunctions.insert(spawnFunc);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
         //Sets dummy to point to a function with name and a dummy implementation
       
         Function *createDummyFunction(string name, Module &M) {
@@ -139,14 +227,16 @@ namespace {
             builder.CreateRetVoid();
             return toReturn;
         }
-      
-        void createDummyCall(Function* fun, Instruction* insertBef) {
-            createDummyCall(fun,insertBef,true);
-        }
 
-        void createDummyCall(Function* fun, Instruction* insertBef, bool before) {
+        void createDummyCall(Function* fun, Instruction* insertBef, int arg) {
+            createDummyCall(fun,insertBef,true,arg);
+        }
+        
+        void createDummyCall(Function* fun, Instruction* insertBef, bool before, int arg) {
             vector<Value*> arglist;
+            arglist.push_back(ConstantInt::get(getGlobalContext(),APInt(32,arg)));
             ArrayRef<Value*> args(arglist);
+            
             CallInst* markCall = CallInst::Create(fun,args);
             if (before)
                 markCall->insertBefore(insertBef);
