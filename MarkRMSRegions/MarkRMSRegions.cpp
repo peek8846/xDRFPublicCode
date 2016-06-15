a2d0b0694f69a4cc59d771adf55b531e57881fe3
zaik <zaik_swe@msn.com>
2016-06-15 22:31:11 +0200

    Added pass to mark RMS with regions (incomplete)

---
+++ MarkRMSRegions/MarkRMSRegions.cpp
@@ -0,0 +1,234 @@
+//===-------------------- Marks nDRF and xDRF regions ---------------------===//
+// Marks the source code with the beginning and end of RMS regions
+//===----------------------------------------------------------------------===//
+// Created at 1/2 -16
+// Jonatan Waern
+//===----------------------------------------------------------------------===//
+
+// #include <sstream>
+#include <iostream>
+#include <string>
+
+// #include <stack>
+// #include <set>
+#include <vector>
+#include <deque>
+// #include <list>
+// #include <map>
+#include <utility>
+// #include <algorithm>
+
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Support/raw_ostream.h"
+//#include "llvm/Support/InstIterator.h"
+#include "llvm/Support/Debug.h"
+
+#include "llvm/ADT/SmallVector.h"
+#include "llvm/ADT/ArrayRef.h"
+// #include "llvm/ADT/APInt.h"
+#include "llvm/ADT/SmallPtrSet.h"
+
+#include "llvm/IR/Instruction.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/Value.h"
+// #include "llvm/IR/Intrinsics.h"
+// #include "llvm/IR/Metadata.h"
+// #include "llvm/IR/CFG.h"
+// #include "llvm/IR/DerivedTypes.h"
+// #include "llvm/IR/Dominators.h"
+// #include "llvm/IR/InstIterator.h"
+#include "llvm/IR/Constants.h"
+// #include "llvm/IR/Attributes.h"
+#include "llvm/IR/NoFolder.h"
+
+// #include "llvm/Analysis/LoopInfo.h"
+#include "llvm/Analysis/CFG.h"
+// #include "llvm/Analysis/AliasAnalysis.h"
+// #include "llvm/Analysis/TargetLibraryInfo.h"
+// #include "llvm/Analysis/MemoryLocation.h"
+// #include "llvm/Analysis/ScalarEvolution.h"
+// #include "llvm/Analysis/ScalarEvolutionExpressions.h"
+
+#include "llvm/Transforms/Utils/BasicBlockUtils.h"
+// #include "llvm/Transforms/Utils/Cloning.h"
+
+#include "llvm/Pass.h"
+
+#define LIBRARYNAME "MarkRMSRegions"
+
+//Define moderately pretty printing functions
+#define PRINTSTREAM errs()
+#define PRINT PRINTSTREAM << "MarkRMSRegions: "
+#define PRINT_DEBUG PRINTSTREAM << "MarkRMSRegions (debug): "
+
+//Verbose prints things like progress
+#define VERBOSE_PRINT(X) DEBUG_WITH_TYPE("verbose",PRINT << X)
+//Light prints things like more detailed progress
+#define LIGHT_PRINT(X) DEBUG_WITH_TYPE("light",PRINT << X)
+//Debug should more accurately print exactly what is happening
+#define DEBUG_PRINT(X) DEBUG_WITH_TYPE("debug",PRINT_DEBUG << X)
+
+using namespace llvm;
+using namespace std;
+
+#define MONXDRF 0
+#define MOXDRF 1
+
+namespace {
+
+    struct MarkRMSRegions : public ModulePass {
+        static char ID;
+        MarkRMSRegions() : ModulePass(ID) {
+        }
+
+    public:
+        virtual void getAnalysisUsage(AnalysisUsage &AU) const{
+        }
+      
+        virtual bool runOnModule(Module &M) {
+	    Function *beginNDRF = createDummyFunction("begin_NDRF",M);
+	    Function *endNDRF = createDummyFunction("end_NDRF",M);
+	    Function *beginXDRF = createDummyFunction("begin_XDRF",M);
+	    Function *endXDRF = createDummyFunction("end_XDRF",M);
+
+            return true;
+        }
+    private:
+
+        void markInitialAcq(Module &M) {
+            Function * RMS_Initial_Acq = M.getFunction("RMS_Initial_Acq");
+            Function * RMS_Final_Release = M.getFunction("RMS_Initial_Acq");
+            if (!RMS_Initial_Acq || !RMS_Final_Release) {
+                VERBOSE_PRINT("No regular locks to mark");
+            }
+                
+            for (auto potentialcall : RMS_Initial_Acq->users()) {
+                bool enclave;
+                if (auto call = dyn_cast<CallInst>(potentialcall)) {
+                    //Find whether it is enclave
+                    enclave = dyn_cast<ConstantInt>(call->getArgOperandUse(1).get())->getZExtValue() == MONXDRF;
+                    //Mark it
+                    
+                }
+            }
+        }
+        
+        //Utility: Checks wether a given callsite contains a call
+        bool isNotNull(CallSite call) {
+            return call.isCall() || call.isInvoke();
+        }
+        
+        //Utility: Checks whether an instruction could be a callsite
+        bool isCallSite(Instruction* inst) {
+            return isNotNull(CallSite(inst));
+        }
+
+        //Finds the functions that may be the entry points of threads
+        //INPUT: The module to analyze and the set into which to insert
+        //the results
+        void findEntryPoints(Module &M, SmallPtrSet<Function*,4> &targetFunctions) {
+            VERBOSE_PRINT("Determining entry points for threads...\n");
+            //For each function that can spawn new threads, find the calls to that function
+            for (StringRef funName : threadFunctions) {
+                VERBOSE_PRINT("Finding functions used by " << funName << "\n");
+                Function *fun = M.getFunction(funName);
+                if (!fun) {
+                    VERBOSE_PRINT("was not used by module\n");
+                } else {
+                    for (auto call = fun->users().begin(); call != fun->users().end(); ++call) {
+                        DEBUG_PRINT("Examining use: " << **call << "\n");
+                        if (dyn_cast<Instruction>(*call) && isCallSite(dyn_cast<Instruction>(*call))) {
+                            CallSite callsite(*call);
+                            for (int opnum = 0; opnum < callsite.getNumArgOperands(); ++opnum) {
+                                Value *funcOp = callsite.getArgOperand(opnum);
+                                DEBUG_PRINT("Examining argument: " << *funcOp << "\n");
+                                //Try to resolve the value into a proper function
+                                while (!isa<Function>(funcOp)) {
+                                    if (isa<ConstantExpr>(funcOp)) {
+                                        ConstantExpr *constOp = dyn_cast<ConstantExpr>(funcOp);
+                                        if (constOp->isCast()) {
+                                            funcOp = constOp->getAsInstruction();
+                                            CastInst *castOp = dyn_cast<CastInst>(funcOp);
+                                            Value* tempOp = funcOp;
+                                            funcOp = castOp->getOperand(0);
+                                            delete(tempOp);
+                                        }
+                                    } else if (isa<CastInst>(funcOp)) {
+                                        CastInst *castOp = dyn_cast<CastInst>(funcOp);
+                                        funcOp = castOp->getOperand(0);
+                                    }
+                                    else { //Unable To Resolve
+                                        DEBUG_PRINT("Failed to resolve argument " << *funcOp
+                                                    << " to a function, remaining type is:" << typeid(*funcOp).name() << "\n");
+                                        break;
+                                    }
+                                }
+                                if (auto spawnFunc = dyn_cast<Function>(funcOp)) {
+                                    VERBOSE_PRINT("Targeting function: " << spawnFunc->getName() << "\n");
+                                    targetFunctions.insert(spawnFunc);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+        //Sets dummy to point to a function with name and a dummy implementation
+      
+        Function *createDummyFunction(string name, Module &M) {
+            if (M.getFunction(name))
+                return M.getFunction(name);
+            //assert(!M.getFunction(name) && "Error creating dummy function, a function with that name already existed");
+            Function *toReturn =
+                cast<Function>(M.getOrInsertFunction(name,
+                                                     FunctionType::get(Type::getVoidTy(getGlobalContext()),
+                                                                       true)));
+            toReturn->addFnAttr(Attribute::NoInline);
+            toReturn->addFnAttr(Attribute::OptimizeNone);
+            BasicBlock *block = BasicBlock::Create(getGlobalContext(),
+                                                   "entry", toReturn);
+            IRBuilder<true, NoFolder> builder(block);
+            // //Use a redundant add as a no-op
+            // builder.CreateBinOp(Instruction::Add,
+            // 		    ConstantInt::get(getGlobalContext(),APInt(32,1)),
+            // 		    ConstantInt::get(getGlobalContext(),APInt(32,1)));
+            builder.CreateRetVoid();
+            return toReturn;
+        }
+
+        void createDummyCall(Function* fun, Instruction* insertBef, int arg) {
+            createDummyCall(fun,insertBef,true,arg);
+        }
+        
+        void createDummyCall(Function* fun, Instruction* insertBef, bool before, int arg) {
+            vector<Value*> arglist;
+            arglist.push_back(ConstantInt::get(getGlobalContext(),APInt(32,arg)));
+            ArrayRef<Value*> args(arglist);
+            
+            CallInst* markCall = CallInst::Create(fun,args);
+            if (before)
+                markCall->insertBefore(insertBef);
+            else
+                markCall->insertAfter(insertBef);
+        }
+    };
+}
+
+char MarkRMSRegions::ID = 0;
+static RegisterPass<MarkRMSRegions> Z("MarkRMS",
+				       "Marks nDRF and XDRF regions on RMS calls",
+				       true,
+				       true);
+
+/* Local Variables: */
+/* mode: c++ */
+/* indent-tabs-mode: nil */
+/* c-basic-offset: 4 */
+/* End: */
