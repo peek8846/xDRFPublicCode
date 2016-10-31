8a9f76f6e28335b3e16304e60b1596de783ca089
Per Ekemark <per.h.ekemark@gmail.com>
2016-10-31 11:09:56 +0100

    remove trailing slash set by pin-env.sh
    The trailing slash in pin-env.sh was redundant and inconsistent
    with other environment scripts in the repo.

--- utility/pin-env.sh
+++ utility/pin-env.sh
@@ -5,11 +5,11 @@
 #   source <path to this script>/pin-env.sh
 
 # After running following variable will be set
-#   PIN_BIN="<pin-prefix>/"
+#   PIN_BIN="<pin-prefix>"
 # and only for the lifetime of the shell.
 
 echo "Setting variables:"
-echo "PIN_BIN=\"$(pwd)/\""
-export PIN_BIN="$(pwd)/"
+echo "PIN_BIN=\"$(pwd)\""
+export PIN_BIN="$(pwd)"
 
 
